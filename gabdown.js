cab416feb5d9e366c10ad67df4bec6dd67c665b2
gabrielamp <29508110+gabrielamp@users.noreply.github.com>
2020-07-04 09:50:11 -0400

    Add files via upload

---
+++ gabdown.js
@@ -0,0 +1,37 @@
+function replaceAll(s, find, replace) {
+  return s.replace(new RegExp(find, 'g'), replace);
+}
+
+//Fix this
+function processString(s) {
+  s = replaceAll(s, String.raw`\\\(`, String.raw`\\(`);
+  s = replaceAll(s, String.raw`\\\)`, String.raw`\\)`);
+  s = replaceAll(s, String.raw`\\\[`, String.raw`\\[`);
+  s = replaceAll(s, String.raw`\\\]`, String.raw`\\]`);
+  return s;
+}
+
+function createCustomElement(name, first, last) {
+  customElements.define(name, class extends HTMLElement {
+    constructor() {
+      super();
+      this.innerHTML = marked(processString(first + this.innerHTML + last));
+    }
+    connectedCallback() {
+      renderMathInElement(this);
+    }
+  });
+}
+
+function gabify(element, gabdown) {
+  element.innerHTML = marked(processString(gabdown));
+  renderMathInElement(element,
+    [
+      {left: "$$", right: "$$", display: true},
+      {left: "\\(", right: "\\)", display: false},
+      {left: "\\[", right: "\\]", display: true},
+      {left: "\\\\(", right: "\\\\)", display: false},
+      {left: "\\\\[", right: "\\\\]", display: true}
+    ]
+  );
+}
