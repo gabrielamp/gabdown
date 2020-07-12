91b1843d58f674a16219a45b03e520e7fff4bf3e
Gabriela Moisescu-Pareja <29508110+gabrielamp@users.noreply.github.com>
2020-07-12 19:41:43 -0400

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-createCustomElement("thm-env", "<b>Theorem. </b><i>",  "</i>");
-createCustomElement("def-env", "<b>Definition. </b>", "");
-createCustomElement("proof-env", "<i>Proof. </i>", " <span style='float:right;'>▢ </span>");
-
-window.onload = () => {
-  fetch("/index.gd")
-    .then(response => response.text())
-    .then(data => gabify(document.body, data));
-};
