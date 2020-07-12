fe06cf24ca148c349e18e8e3c7ed788c9b73c675
Gabriela Moisescu <gmp@Gabrielas-MacBook-Pro.local>
2020-07-12 19:47:40 -0400

    Fix path

--- autorender.js
+++ autorender.js
@@ -3,7 +3,7 @@ createCustomElement("def-env", "<b>Definition. </b>", "");
 createCustomElement("proof-env", "<i>Proof. </i>", " <span style='float:right;'>▢ </span>");
 
 window.onload = () => {
-  fetch("/index.gd")
+  fetch("index.gd")
     .then(response => response.text())
     .then(data => gabify(document.body, data));
 };
